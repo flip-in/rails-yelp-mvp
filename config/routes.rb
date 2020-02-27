8af09c75b9cbaaf031773c5a5980b387a1b6c61b
Will Price <pricew86@gmail.com>
2020-02-27 12:59:40 +0800

    adjusted routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'restaurants#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :restaurants, except: [:destroy] do
+  resources :restaurants, except: [:destroy, :index] do
     resources :reviews, only: [:new, :create]
   end
 end
